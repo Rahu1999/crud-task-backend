044be1681706efc32f2f07323f598e0953fb75fe
rahu1999 <rahulrajbhar19999@gmail.com>
2023-12-22 12:00:33 +0530

    mongo url added from env

--- database/mongoose.js
+++ database/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://127.0.0.1:27017/taskmanagerdb')
+mongoose.connect(process.env.MONGO_URL)
 .then(()=>{
     console.log("DN connected successfully")
 })
